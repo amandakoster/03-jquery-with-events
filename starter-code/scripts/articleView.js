9996ce673352d92880ee279575439d5dd2f005eb
Amanda Koster <amandakoster@gmail.com>
2017-05-10 15:47:46 -0700

    tasks 1 and 2 complete

--- starter-code/scripts/articleView.js
+++ starter-code/scripts/articleView.js
@@ -32,22 +32,10 @@ articleView.populateFilters = function() {
 
 articleView.handleAuthorFilter = function() {
   $('#author-filter').on('change', function() {
-    // REVIEW: Inside this function, "this" is the element that triggered the event handler function we're
-    //         defining. "$(this)" is using jQuery to select that element, so we can chain jQuery methods
-    //         onto it.
     if ($(this).val()) {
-      // TODO: If the select box was changed to an option that has a value, we need to hide all the articles,
-      //       and then show just the ones that match for the author that was selected.
-      //       Use an "attribute selector" to find those articles written by the selected author, and fade them in for the reader.
-
-      var articleName = $(this).attr('data-author');
+      var authorName = $(this).val()
       $('article').hide()
-      //add a filter that will filter articles by author
-      ($('#category-filter author[value="' + category + '"]'))
-      // $('#category-filter').find(optionTag)
-      $('#author-filter').fadeIn()
-
-
+      $('article[data-author= "' + authorName + '"]').fadeIn(750)
     } else {
       $('#category-filter').val('');
     }
@@ -89,5 +77,6 @@ articleView.setTeasers = function() {
 // TODO: Call all of the above functions, once we are sure the DOM is ready.
 $(document).ready(function() {
   articleView.populateFilters();
+  articleView.handleAuthorFilter();
 
 })
