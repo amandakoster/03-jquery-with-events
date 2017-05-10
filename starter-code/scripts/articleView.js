4dc7891f85a4f99f283bdd591be6751330dbd718
Amanda Koster <amandakoster@gmail.com>
2017-05-10 16:00:05 -0700

    tasks 1 and 2 complete

--- starter-code/scripts/articleView.js
+++ starter-code/scripts/articleView.js
@@ -1,26 +1,17 @@
 'use strict';
 
-// Configure a view object, to hold all our functions for dynamic updates and article-related event handlers.
 var articleView = {};
 
 articleView.populateFilters = function() {
   $('article').each(function() {
     var authorName, category, optionTag;
     if (!$(this).hasClass('template')) {
-      // REVIEW: We need to take every author name from the page, and make it an option in the Author filter.
-      //       To do so, Build an `option` DOM element that we can append to the author select box.
-      //       Start by grabbing the author's name from an attribute in `this` article element,
-      //       and then use that bit of text to create the option tag (in a variable named `optionTag`),
-      //       that we can append to the #author-filter select element.
       authorName = $(this).attr('data-author');
       optionTag = '<option value="' + authorName + '">' + authorName + '</option>';
 
       if ($('#author-filter option[value="' + authorName + '"]').length === 0) {
         $('#author-filter').append(optionTag);
       }
-      // REVIEW: Similar to the above, but...
-      //       Avoid duplicates! We don't want to append the category name if the select
-      //       already has this category as an option!
       category = $(this).attr('data-category');
       optionTag = '<option value="' + category + '">' + category + '</option>';
       if ($('#category-filter option[value="' + category + '"]').length === 0) {
@@ -48,6 +39,17 @@ articleView.handleCategoryFilter = function() {
   //       When the blank (default) option is selected, show all the articles, except for the template.
   //       Be sure to reset the #author-filter while you are at it!
 
+};
+articleView.handleAuthorFilter = function() {
+  $('#author-filter').on('change', function() {
+    if ($(this).val()) {
+      var authorName = $(this).val()
+      $('article').hide()
+      $('article[data-author= "' + authorName + '"]').fadeIn(750)
+    } else {
+      $('#category-filter').val('');
+    }
+  });
 };
 
 articleView.handleMainNav = function() {
